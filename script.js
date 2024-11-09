956c9c9bef9a17a05de9cead61a4e448aaf23abd
Felpopinho <157515144+Felpopinho@users.noreply.github.com>
2024-11-09 06:22:17 +0000

    All commit

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const inputEquipeSearch = document.getElementById("inputSearchEquipe")
 const pokemonUm = document.getElementById("pokemonUm")
 const pokemonDois = document.getElementById("pokemonDois")
 
+const desc = document.getElementById("descricao")
+
 var pokemonAtualCont = 1
 
 var pokemonUmSelecionado = false;
@@ -18,6 +20,26 @@ var pokemonDoisSelecionado = false;
 const pokemonBatalha = ["", ""];
 
 var seuTurno = true;
+
+var atackSound = new Audio("./assets/JumpKick.wav")
+var erroSound = new Audio("./assets/Cut.wav")
+var batalhaMusic1 = new Audio("./assets/batalha1.mp3")
+var batalhaMusic2 = new Audio("./assets/batalha2.mp3")
+var batalhaMusic3 = new Audio("./assets/batalha3.mp3")
+var vitoriaMusic1 = new Audio("./assets/vitoriaMusic1.mp3")
+var vitoriaMusic2 = new Audio("./assets/vitoriaMusic2.mp3")
+var vitoriaMusic3 = new Audio("./assets/vitoriaMusic3.mp3")
+var derrotaMusic = new Audio("./assets/derrotaMusic.mp3")
+derrotaMusic.loop = true
+
+const batalhaMusicArr = [batalhaMusic1, batalhaMusic2, batalhaMusic3]
+batalhaMusicArr.forEach(audio => {
+    audio.loop = true
+})
+const vitoriaMusicArr = [vitoriaMusic1, vitoriaMusic2, vitoriaMusic3]
+vitoriaMusicArr.forEach(audio => {
+    audio.loop = true
+})
 
 async function searchPokemon(section){
 
@@ -138,9 +160,17 @@ function selectPokemonContainer(n){
         pokemonAtualCont = 2
     }
 }
-
+var musicBatalhaRandon;
 function iniciarBatalha(){
     seuTurno = true;
+
+    document.querySelectorAll(".batalha").forEach(e =>{
+        e.style.display = "flex"
+    })
+
+    document.getElementById("btnFinalizar").style.display = "none"
+    document.getElementById("seuTurno").style.display = "flex"
+    document.getElementById("descAcao").style.display = "none"
 
     document.querySelector(".equipe_pokemons").style.display = "none"
     document.getElementById("btnStartBattle").style.display = "none"
@@ -150,11 +180,12 @@ function iniciarBatalha(){
     let seuPokemon = pokemonBatalha[0]
     let pokemonInimigo = pokemonBatalha[1]
 
-    var musica = new Audio('./assets/music.mp3')
-    musica.play()
+    musicBatalhaRandon = Math.floor(Math.random()*(2.9 - 0)+0)
+    batalhaMusicArr[musicBatalhaRandon].currentTime = 0
+    batalhaMusicArr[musicBatalhaRandon].play()
 
-    document.getElementById("inimigoImg").src = pokemonInimigo.sprites.front_default
-    document.getElementById("seuImg").src = seuPokemon.sprites.back_default
+    document.getElementById("inimigoImg").src = pokemonInimigo.sprites.other.showdown.front_default
+    document.getElementById("seuImg").src = seuPokemon.sprites.other.showdown.back_default
     document.getElementById("inimigoVida").innerHTML = pokemonInimigo.name
     document.getElementById("seuVida").innerHTML = seuPokemon.name
     document.getElementById("inimigoBarra").value = `${pokemonInimigo.stats[0].base_stat}`
@@ -169,9 +200,12 @@ var tipoVitima;
 let atq;
 let def;
 let sAtq;
+let accuracy;
+let evasion;
 
-function abrirAtaques(){
+function atacar(){
 
+    var vida;
     if(seuTurno === true){
         vida = "inimigoBarra";
         tipoAtacante = pokemonBatalha[0].types[0].type.name;
@@ -179,6 +213,20 @@ function abrirAtaques(){
         atq = pokemonBatalha[0].stats[1].base_stat;
         def = pokemonBatalha[1].stats[2].base_stat;
         sAtq = pokemonBatalha[0].stats[3].base_stat;
+        accuracy = pokemonBatalha[0].stats[5].base_stat;
+        evasion = pokemonBatalha[1].stats[4].base_stat
+
+        document.getElementById("seuTurno").style.display = "none"
+        document.getElementById("descAcao").style.display = "flex"
+        desc.innerHTML = `Você ataca com o ${pokemonBatalha[0].name}`
+        const textoArray = desc.innerHTML.split('');
+        desc.innerHTML = ' ';
+
+        textoArray.forEach(function(letra, i){   
+            setTimeout(function(){
+                desc.innerHTML += letra;
+            }, 75 * i)
+        });
         seuTurno = false
     } else{
         vida = "seuBarra";
@@ -187,45 +235,89 @@ function abrirAtaques(){
         atq = pokemonBatalha[1].stats[1].base_stat;
         def = pokemonBatalha[0].stats[2].base_stat;
         sAtq = pokemonBatalha[1].stats[3].base_stat;
+        accuracy = pokemonBatalha[1].stats[5].base_stat;
+        evasion = pokemonBatalha[0].stats[4].base_stat
         seuTurno = true
     }
 
-    let stab = pokemonBatalha[0].types[0].type.name === pokemonBatalha[1].types[0].type.name ? 1.5 : 1
+    let acerta = acertarAtaque()
 
-    let bonus = setBonus();
+    setTimeout(() => {
+        if (acerta===false){
+            desc.innerHTML = `${seuTurno == false ? pokemonBatalha[0].name : pokemonBatalha[1].name} erra o ataque`
+            const textoArray = desc.innerHTML.split('');
+            desc.innerHTML = '';
+            textoArray.forEach(function(letra, i){   
+                setTimeout(function(){
+                    desc.innerHTML += letra;
+                }, 75 * i)
+            });
 
-    let na = Math.floor(Math.random() * (100 - 85 + 1)) + 85
+            erroSound.play()
+            seuTurno === false ? document.querySelector(".pokemonInimigo").style.right= "80px" : document.querySelector(".seuPokemon").style.left= "100px"
+            seuTurno === false ? document.getElementById("gifatkInimigo").style.left= "80px" : document.getElementById("gifatkSeu").style.right= "100px"
+            document.getElementById(`${seuTurno == false ? "gifatkInimigo" : "gifatkSeu"}`).style.display = "block"
+            setTimeout(()=>{
+                document.getElementById(`${seuTurno == false ? "gifatkInimigo" : "gifatkSeu"}`).style.display = "none"
+                seuTurno === false ? document.getElementById("gifatkInimigo").style.left= "0px" : document.getElementById("gifatkSeu").style.right= "0px"
+            }, "1000")
+            
+            setTimeout(() => {
+                if (seuTurno === false){
+                    document.querySelector(".pokemonInimigo").style.right= "0px"
+                    return turnoOponente()
+                } else{
+                    document.querySelector(".seuPokemon").style.left= "0px"
+                    document.getElementById("descAcao").style.display = "none"
+                    document.getElementById("seuTurno").style.display = "flex"
+                }
+            }, "4000")
+            
+        } else{
+            let bonus = setBonus();
+            desc.innerHTML = `${seuTurno == false ? pokemonBatalha[0].name : pokemonBatalha[1].name} acerta um ataque ${bonus === 1/2 ? "pouco efetivo" : bonus === 2 ? "super efetivo" : bonus = 0 ? "sem efeito" : ""}`
+            const textoArray = desc.innerHTML.split('');
+            desc.innerHTML = ' ';
+            textoArray.forEach(function(letra, i){   
+                setTimeout(function(){
+                    desc.innerHTML += letra;
+                }, 75 * i)
+            });
+            let stab = pokemonBatalha[0].types[0].type.name === pokemonBatalha[1].types[0].type.name ? 1.5 : 1
+            let na = Math.floor(Math.random() * (100 - 85 + 1)) + 85
+            let dano = (((((1 * 2/5) + 2) * sAtq * atq/def)/50 + 2) * stab * bonus * na/100)
 
-    let dano = (((((1 * 2/5) + 2) * sAtq * atq/def)/50 + 2) * stab * bonus * na/100)
+            atackSound.play()
+            document.getElementById(`${seuTurno == false ? "gifatkInimigo" : "gifatkSeu"}`).style.display = "block"
+            setTimeout(()=>{
+                document.getElementById(`${seuTurno == false ? "gifatkInimigo" : "gifatkSeu"}`).style.display = "none"
+            }, "1000")
+            
+            setTimeout(()=>{
+                if(vida === "inimigoBarra"){
+                        document.getElementById(vida).value = parseInt(document.getElementById(vida).value - dano);
+                } else{
+                    document.getElementById(vida).value = parseInt(document.getElementById(vida).value - dano);
+                    document.getElementById("seuVidaText").innerHTML = `${document.getElementById(vida).value}/${pokemonBatalha[0].stats[0].base_stat}`
+                }
 
-    console.log(dano)
-
-    document.getElementById(vida).value = document.getElementById(vida).value - dano;
-
-    if (document.getElementById(vida).value <= 0){
-        fimBatalha()
-    }
-    if (seuTurno === false){
-        document.getElementById("seuTurno").style.display = "none"
-        document.getElementById("descAcao").style.display = "flex"
-
-        const desc = document.getElementById("descricao")
-        desc.innerHTML = `O ${pokemonBatalha[0].name} ataca`
-        const textoArray = desc.innerHTML.split('');
-        desc.innerHTML = ' ';
-
-        textoArray.forEach(function(letra, i){   
-            setTimeout(function(){
-                desc.innerHTML += letra;
-            }, 75 * i)
-        })
-        setTimeout(() => {
-            turnoOponente()
-        }, "5000");
-    } else{
-        document.getElementById("descAcao").style.display = "none"
-        document.getElementById("seuTurno").style.display = "flex"
-    }
+                if (document.getElementById("seuBarra").value <= 0 || document.getElementById("inimigoBarra").value <= 0){
+                    if (seuTurno === false){
+                        return fimBatalha(pokemonBatalha[0])
+                    } else{
+                        return fimBatalha(pokemonBatalha[1])
+                    }
+                } else{
+                    if (seuTurno === false){
+                        turnoOponente()
+                    } else{
+                        document.getElementById("descAcao").style.display = "none"
+                        document.getElementById("seuTurno").style.display = "flex"
+                    }
+                }
+            }, "4000")
+        }
+    }, "3000")
 
 }
 
@@ -233,8 +325,7 @@ function turnoOponente(){
     document.getElementById("seuTurno").style.display = "none"
     document.getElementById("descAcao").style.display = "flex"
 
-    const desc = document.getElementById("descricao")
-    desc.innerHTML = `O ${pokemonBatalha[1].name} ataca`
+    desc.innerHTML = `O inimigo ataca com o ${pokemonBatalha[1].name}`
     const textoArray = desc.innerHTML.split('');
     desc.innerHTML = ' ';
 
@@ -243,18 +334,74 @@ function turnoOponente(){
             desc.innerHTML += letra;
         }, 75 * i)
     })
-
-    setTimeout(() => {
-        abrirAtaques()
-    }, "5000");
+    atacar()
 }
 
-function fimBatalha(){
+let musicVitoriaRandon;
 
+function fimBatalha(vencedor){
+    let perdedor;
+    musicVitoriaRandon = Math.floor(Math.random()*(2.9 - 0)+0)
+    vitoriaMusicArr[musicVitoriaRandon].currentTime = 0
+    document.querySelectorAll(".batalha").forEach(e =>{
+        e.style.display = "none"
+    })
+    document.getElementById("fimBatalha").style.display = "flex"
+    document.getElementById("pokemonVencedor").src = vencedor.sprites.other.showdown.front_default
+
+    document.getElementById("seuTurno").style.display = "none"
+    document.getElementById("descAcao").style.display = "flex"
+
+    document.querySelector(".display_pokemons").style.backgroundImage = "url('./assets/background_pokemon.avif')"
+    document.querySelector(".display_pokemons").style.backgroundPosition = "bottom"
+
+    document.getElementById("btnFinalizar").style.display = "block"
+
+    batalhaMusicArr[musicBatalhaRandon].pause()
+
+    if(vencedor === pokemonBatalha[0]){
+        perdedor = pokemonBatalha[1]
+        vitoriaMusicArr[musicVitoriaRandon].play()
+        desc.innerHTML = `Parabens! Você ganhou a batalha contra ${perdedor.name}`
+        const textoArray = desc.innerHTML.split('');
+        desc.innerHTML = ' ';
+
+        textoArray.forEach(function(letra, i){   
+            setTimeout(function(){
+                desc.innerHTML += letra;
+            }, 75 * i)
+        })
+    } else{
+        perdedor = pokemonBatalha[0]
+        derrotaMusic.play()
+        desc.innerHTML = `Oh não! Infelizmente você perdeu a batalha contra ${perdedor.name}`
+        const textoArray = desc.innerHTML.split('');
+        desc.innerHTML = ' ';
+
+        textoArray.forEach(function(letra, i){   
+            setTimeout(function(){
+                desc.innerHTML += letra;
+            }, 75 * i)
+        })
+    }
+}
+
+function acertarAtaque(){
+    let calcMultiplicador = (Math.random()*(2 - 0.3) + 0.3).toString()
+    let multiplicador = parseFloat(calcMultiplicador.slice(0, 4))
+    
+    let chanceAcerto = multiplicador * (accuracy/evasion)
+
+    console.log(chanceAcerto)
+
+    if(chanceAcerto >= 1){
+        return true
+    } else{
+        return false
+    }
 }
 
 function setBonus(){
-
     //normal
     if(tipoAtacante === "normal"){
         if(tipoVitima === "rock" || tipoVitima === "steel"){
@@ -289,8 +436,8 @@ function setBonus(){
     }
 
     //eletric
-    if(tipoAtacante === "eletric"){
-        if(tipoVitima === "eletric" || tipoVitima === "grass" || tipoVitima === "dragon"){
+    if(tipoAtacante === "electric"){
+        if(tipoVitima === "electric" || tipoVitima === "grass" || tipoVitima === "dragon"){
             return 1/2
         } else if(tipoVitima === "water" || tipoVitima === "flying"){
             return 2
@@ -353,7 +500,7 @@ function setBonus(){
     if(tipoAtacante === "ground"){
         if(tipoVitima === "grass" || tipoVitima === "bug"){
             return 1/2
-        } else if(tipoVitima === "fire" || tipoVitima === "eletric" || tipoVitima === "poison" || tipoVitima === "rock" || tipoVitima === "steel"){
+        } else if(tipoVitima === "fire" || tipoVitima === "electric" || tipoVitima === "poison" || tipoVitima === "rock" || tipoVitima === "steel"){
             return 2
         } else if(tipoVitima === "flying"){
             return 0
@@ -364,7 +511,7 @@ function setBonus(){
 
     //flying
     if(tipoAtacante === "flying"){
-        if(tipoVitima === "eletric" || tipoVitima === "rock" || tipoVitima === "steel"){
+        if(tipoVitima === "electric" || tipoVitima === "rock" || tipoVitima === "steel"){
             return 1/2
         } else if(tipoVitima === "grass" || tipoVitima === "fighting" || tipoVitima === "bug"){
             return 2
@@ -447,7 +594,7 @@ function setBonus(){
 
     //steel
     if(tipoAtacante === "steel"){
-        if(tipoVitima === "fire" || tipoVitima === "water" || tipoVitima === "eletric" || tipoVitima === "steel"){
+        if(tipoVitima === "fire" || tipoVitima === "water" || tipoVitima === "electric" || tipoVitima === "steel"){
             return 1/2
         } else if(tipoVitima === "bug" || tipoVitima === "ice" || tipoVitima === "fairy"){
             return 2
@@ -471,4 +618,23 @@ function setBonus(){
 
 function abrirMochila(){
 
+}
+
+function resetBattle(){
+    vitoriaMusicArr[musicVitoriaRandon].pause()
+    derrotaMusic.pause()
+
+    document.querySelector(".batalha_pokemon").style.display = "none"
+    document.getElementById("btnStartBattle").style.display = "flex"
+    document.querySelector(".equipe_pokemons").style.display = "flex"
+    document.querySelector(".searchPokemon").style.display = "block"
+
+    document.getElementById("fimBatalha").style.display = "none"
+    document.getElementById("pokemonVencedor").src = ""
+
+    document.getElementById("seuTurno").style.display = "none"
+    document.getElementById("descAcao").style.display = "none"
+
+    document.querySelector(".display_pokemons").style.backgroundImage = "url('./assets/d9spuwer2c491.webp')"
+    document.querySelector(".display_pokemons").style.backgroundPosition = "center"
 }
